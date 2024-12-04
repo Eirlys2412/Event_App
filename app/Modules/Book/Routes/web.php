fa16dcd823e2461b90184761f87e70782cdb23d9
lqtuan123 <lqtuan0102@gmail.com>
2024-12-04 19:20:29 +0700

    #42 update_module

--- app/Modules/Book/Routes/web.php
+++ app/Modules/Book/Routes/web.php
@@ -13,6 +13,7 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
     // Routes for Book
     Route::resource('books', BookController::class)->parameters(['books' => 'id']);
     Route::get('books/{id}', [BookController::class, 'show'])->where('id', '[0-9]+')->name('books.show');
+    Route::delete('/admin/books/{bookId}/resource/{resourceId}', [BookController::class, 'removeResource'])->name('books.removeResource');
     Route::post('books_status', [BookController::class, 'bookStatus'])->name('books.status');
     Route::get('books_search', [BookController::class, 'bookSearch'])->name('books.search');
 
