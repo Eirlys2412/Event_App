65cdff6a653fd40d770bd6f95589bfb2bb62d686
hovietsang <hovietsang2003@gmail.com>
2024-12-26 17:14:37 +0700

    #75 - create_event_group

--- app/Modules/Events/Routes/web.php
+++ app/Modules/Events/Routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Modules\Events\Controllers\EventTypeController;
 use App\Modules\Events\Controllers\EventController;
+use App\Modules\Events\Controllers\EventGroupController;
 
 Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
     Route::prefix('eventtype')->name('event_type.')->group(function () {
@@ -24,5 +25,14 @@ Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
         Route::put('/update/{id}', [EventController::class, 'update'])->name('update');
         Route::delete('/destroy/{id}', [EventController::class, 'destroy'])->name('destroy');
         Route::delete('/{eventId}/resource/{resourceId}', [EventController::class, 'removeResource'])->name('resource.destroy');
+    });
+
+    Route::prefix('eventgroup')->name('event_group.')->group(function () {
+        Route::get('/', [EventGroupController::class, 'index'])->name('index');
+        Route::get('/create', [EventGroupController::class, 'create'])->name('create');
+        Route::post('/store', [EventGroupController::class, 'store'])->name('store');
+        Route::get('/edit/{id}', [EventGroupController::class, 'edit'])->name('edit');
+        Route::put('/update/{id}', [EventGroupController::class, 'update'])->name('update');
+        Route::delete('/destroy/{id}', [EventGroupController::class, 'destroy'])->name('destroy');
     });
 });
